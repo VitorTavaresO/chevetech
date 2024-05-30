143d1a2bd790216d586e6f994ef39ab5dd32e000
Vitor Tavares <vitortavares.o55@gmail.com>
2024-05-30 18:28:39 -0300

    Reorganized to create a .exe file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import sys
+from kivy.resources import resource_add_path
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.clock import Clock
@@ -5,7 +8,7 @@ from datetime import datetime
 
 class App(MDApp):
     def build(self):
-        self.screen = Builder.load_file('interface.kv')
+        self.screen = Builder.load_file("interface.kv")
         Clock.schedule_interval(self.update_time, 1)
         return self.screen
 
@@ -14,4 +17,8 @@ class App(MDApp):
         current_time = now.strftime("%H:%M\n%d/%m/%Y")
         self.screen.ids.datetime_label.text = current_time
 
-App().run()
+if __name__ == '__main__':
+    if hasattr(sys, '_MEIPASS'):
+        resource_add_path(os.path.join(sys._MEIPASS))
+    
+    App().run()
