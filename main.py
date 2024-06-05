e3e2fb2386191386d51b1cb986c598922968a1fe
Vitor Tavares <vitortavares.o55@gmail.com>
2024-06-05 19:51:48 -0300

    Windows Size Config Write Defined

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import os
 import sys
 from kivy.resources import resource_add_path
 from kivy.lang import Builder
+from kivy.config import Config
 from kivymd.app import MDApp
 from kivy.clock import Clock
 from datetime import datetime
+
+# Defina o tamanho da janela antes de importar qualquer coisa do Kivy
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '600')
+Config.set('graphics', 'resizable', False)  # Torne a janela não redimensionável
+Config.write()
 
 class App(MDApp):
     def build(self):
