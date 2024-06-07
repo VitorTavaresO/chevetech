7174922b036e8add0e9cc82d23d1fc87274b64fb
Vitor Tavares <vitortavares.o55@gmail.com>
2024-06-06 23:00:15 -0300

    Starting Settings Screen

--- main.py
+++ main.py
@@ -33,7 +33,13 @@ class FilesScreen(MDScreen):
     pass
 
 class SettingsScreen(MDScreen):
-    pass
+    def toggle_button_text(self, text, button):
+        if text.text == "OFF":
+            text.text = "ON"
+            button.md_bg_color = (0.118, 0.678, 0.298, 1)
+        else:
+            text.text = "OFF"
+            button.md_bg_color = (0.1, 0.1, 0.1, 1)
 
 class App(MDApp):
     def build(self):
